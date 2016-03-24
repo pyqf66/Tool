10c44e65b7e29adbb0c435fbbe7b23fd33caf8fa
pyqf66 <zyf3282@msn.cn>
2016-03-24 15:44:22 +0800

    更新短信验证码模块

--- common/util/GetSms.py
+++ common/util/GetSms.py
@@ -27,7 +27,9 @@ class GetSms(object):
         redis_key = str(redis_key_phone_number) + "authUserSendCode"
         # 由于取验证码时获取的是二进制数据，所以字符串化以后切片取值
         redis_value = str(redis_object.get_redis_data(redis_key))
-        sms_num = redis_value[-5:-1]
+        sms_num = "无对应手机号的短信验证码"
+        if redis_object.get_redis_data(redis_key) is not None:
+            sms_num = redis_value[-5:-1]
         return sms_num
 
     def get_sms_num_from_db(self, phone_number):
@@ -36,5 +38,7 @@ class GetSms(object):
         mysql_object = MysqlHandler(mysql_user=self.__mysql_user, mysql_password=self.__mysql_password, mysql_host=self.__mysql_host,
                                     mysql_port=self.__mysql_port, mysql_db=self.__mysql_db)
         data_list = mysql_object.get_mysql_data(select_sql)
-        sms_num = data_list[0][0]
+        sms_num = "无对应手机号的短信验证码"
+        if data_list != []:
+            sms_num = data_list[0][0]
         return sms_num
